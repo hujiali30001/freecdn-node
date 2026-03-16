7feb22390f5b10dc464f648a7b477004fc1679c3
hujiali30001 <79893530@qq.com>
2026-03-17 04:53:10 +0800

    chore: bump version to v0.9.1

--- internal/const/const.go
+++ internal/const/const.go
@@ -1,10 +1,10 @@
 package teaconst
 
 const (
-	Version = "0.9.0"
+	Version = "0.9.1"
 
 	// GoEdgeVersion 是本项目基于的 GoEdge 上游版本
-	GoEdgeVersion = "0.9.0"
+	GoEdgeVersion = "0.9.1"
 
 	ProductName = "Edge Node"
 	ProcessName = "edge-node"
